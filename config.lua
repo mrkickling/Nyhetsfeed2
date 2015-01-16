c6eb428a1a59b07fac947b0d8e3cc90cd80efeac
mrkickling <loxdalen@gmail.com>
2015-01-16 17:41:47 +0100

    Introducing composer library

--- config.lua
+++ config.lua
@@ -3,8 +3,7 @@ application =
 
 	content =
 	{
-		width = 320,
-		height = 480, 
+		
 		scale = "letterBox",
 		fps = 30,
 		
--- main.lua
+++ main.lua
@@ -5,3 +5,16 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+W, H = display.contentWidth, display.contentHeight
+
+local background = display.newRect(0,0, W, H)
+background.anchorX, background.anchorY = 0, 0
+background:setFillColor(0.2,0.21,0.21)
+
+local topBar = display.newRect(0, 0, W, H*0.15)
+topBar.anchorX, topBar.anchorY = 0,0
+topBar:setFillColor(0.41,0.41,0.41)
+
+local bottomBar = display.newRect(0, H, W, H*0.15)
+bottomBar.anchorX, bottomBar.anchorY = 0,1
+bottomBar:setFillColor(0.41,0.41,0.41)
